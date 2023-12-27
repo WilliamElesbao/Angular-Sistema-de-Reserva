f5979f3d50539169f33acc0458170fba0769792b
WilliamElesbao <william.elesbao.2000@gmail.com>
2023-12-27 16:02:04 -0300

    deixado dinamico a renderizacao das ilhas e mesas proximas a janela conforme os dados vindos do arquivo dados_ilhas_janela.json

--- src/app/ilha-janela/ilha-janela.component.ts
+++ src/app/ilha-janela/ilha-janela.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import info_ilhas_mesas from '../../assets/data/dados_ilhas_janela.json'
+
+interface Mesa {
+  id: number;
+  nome: string;
+}
+
+interface Ilha {
+  id: number;
+  nome: string;
+  mesas: Mesa[];
+}
 
 @Component({
   selector: 'app-ilha-janela',
@@ -7,9 +19,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IlhaJanelaComponent implements OnInit {
 
+  btnContent: string = 'Reservar'
+  ilhas: Ilha[] = []
+
   constructor() { }
 
   ngOnInit(): void {
+
+    if(info_ilhas_mesas){
+      this.ilhas = info_ilhas_mesas.dados_ilha
+      // console.log(this.ilhas)
+    }
+
   }
 
 }
